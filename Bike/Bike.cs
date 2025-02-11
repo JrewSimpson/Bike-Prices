8c1ae1d7c4584ce23d44d546e95231e7849a3415
simps <simps@DESKTOP-9QJ844T>
2025-02-11 15:35:05 -0500

    Add project files.

---
+++ Bike/Bike.cs
@@ -0,0 +1,102 @@
+﻿// CIS 199-50
+// Grading ID: A1026
+// Due Date: 12/09/2023
+// Program stores and lists the different bikes for rent currently staying at the marina
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bike
+{
+    public class Bike
+    {
+        public const int zero = 0;
+        private string _bikeName;
+        private int _bikeId;
+        private double _bikePrice;
+        private string _bikeRenter;
+        private string _bikeType;
+        private bool _isElectric;
+        //#1 - Put needed backing fields here. Remember: Some properties may be
+        // auto-implemenetd so only create backing fields for ones needed
+
+
+
+        public string BikeName
+        {
+            get { return _bikeName; }
+            set { _bikeName = value; }
+        }
+        public string BikeRenter
+        {
+            get { return _bikeRenter;  }
+            set { _bikeRenter = value; }
+        }
+        public string BikeType
+        {
+            get { return _bikeType; }
+            set { _bikeType = value; }
+        }
+        public int BikeID
+        {
+            get { return _bikeId; }
+            set
+            {
+                if (value >= 00001 && value <= 99999) _bikeId = value;
+                else _bikeId = 00000;
+            }
+
+
+        }
+        public double BikePrice
+        {
+            get { return _bikePrice; }
+            set
+            {
+                if (value >= zero) _bikePrice = value;
+                else _bikePrice = zero;
+            }
+
+
+        }
+
+        public Bike(string bikeName, int bikeID, double bikePrice, string bikeRenter, string bikeType)
+        {
+            // #7 - Put your code here for constructor. Be sure your parameters get validated through properties
+            BikeName = bikeName;
+            BikeID = bikeID;
+            BikePrice = bikePrice;
+            BikeRenter = bikeRenter;
+            BikeType = bikeType;
+
+            IsElectric();
+            
+        }
+        private bool isElectric;
+
+        public void IsNotElectricBike()
+        {   // #8 - Put your code here to determine if the bike is not electric
+            _isElectric = false;
+        }
+        public void IsElectricBike()
+        {   // #9 Put your code here to determine if the bike is electric
+            _isElectric = true;
+        }
+        public bool IsElectric()
+        {
+            // #10 - Put your code here to determine if the bike is electric or not
+            return _isElectric;
+        }
+        public override string ToString()
+        {
+            return $"Bike Name: {BikeName.ToString()} {Environment.NewLine}" +
+                   $"Bike ID: {BikeID.ToString()} {Environment.NewLine}" +
+                   $"Bike Price: {BikePrice.ToString("C")} {Environment.NewLine}" +
+                   $"Bike Renter: {BikeRenter.ToString()} {Environment.NewLine}" +
+                   $"Bike Type: {BikeType.ToString()} {Environment.NewLine}" +
+                   $"Electric Bike?: {_isElectric.ToString()} {Environment.NewLine}";
+        }
+    }
+}
---
+++ Bike/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace Bike
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // #1 - Put code here to construct 5 bikes
+
+            Bike bike1 = new Bike("Thunderbolt", 45639, 25.00, "Alex Strong", "Road");
+            Bike bike2 = new Bike("Sugeno", 56390, 30.00, "Johny Cash", "Mountain");
+            Bike bike3 = new Bike("Mongus", 95976, 20.00, "Miley Cyrus", "Street");
+            Bike bike4 = new Bike("Raodrunner", 39001, 35.00, "Lady Gaga", "Terrain");
+            Bike bike5 = new Bike("Scorpion", 67392, 30.00, "Bradley Cooper", "Mountain");
+
+            Bike[] bikes = { bike1, bike2, bike3, bike4, bike5 };
+
+            bike2.IsElectricBike();
+
+            WriteLine("Bikes we Rent:");
+            WriteLine("--------------");
+
+            //#2 - Call the BikeDisplay method to display the bike objects
+            BikeDisplay(bikes);
+            
+        }
+
+        public static void BikeDisplay(Bike[] bikes)
+        {
+            // #3 - Use a foreach loop to dislay the bike objects
+            foreach (Bike bike in bikes)
+            {
+                Console.WriteLine(bike);
+            }
+        }
+    }
+}
